3a7ba9504f93cf8f237026b3c67437d06ecf7e74
orozcoding <orozcoding@gmail.com>
2022-01-11 04:44:31 -0400

    add css loadres

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,19 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html'
     }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
